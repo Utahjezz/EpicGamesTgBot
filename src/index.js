4f02d86af7b4e647f3748f9f036e203e31f8ebcb
giacomo.gezzi <giacomo@ilmeteo.it>
2020-03-15 19:30:19 +0100

    first integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import * as functions from 'firebase-functions';
+import TelegramBot from 'node-telegram-bot-api';
+import app from './express-app.js';
 
-const message = () => {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('from compiled JavaScript via Babel on Cloud Functions!');
-        }, 3000);
-    });
-};
+function checkTgMessage(request) {
+    return request.body
+        && request.body.message
+        && request.body.message.chat
+        && request.body.message.chat.id
+        && request.body.message.from
+        && request.body.message.from.first_name;
+}
 
-export const babel = functions.https.onRequest(async (req, res) => {
-    const world = await message();
-    res.status(200).send(`Hello ${world}`);
+const bot = new TelegramBot("");
+
+
+app.post('/helloworld', async (req, res) => {
+    console.log("start");
+    if (checkTgMessage(req)) {
+        const chatId = req.body.message.chat.id;
+        const {firstName} = req.body.message.from;
+
+        await bot.sendMessage(chatId, "Hello bomber");
+        console.log("end")
+        return res.status(200).send({});
+    }
+    return res.status(200).send({ status: 'not a telegram message' });
 });
+
+export const router = functions.https.onRequest(app);
